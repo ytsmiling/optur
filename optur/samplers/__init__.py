8b791cb19e338c1397d3c83746d69dabe9870243
Yusuke Tsuzuku <ytsmiling@gmail.com>
2021-12-23 22:23:33 +0900

    Add SamplerConfig and create_sampler().

--- optur/samplers/__init__.py
+++ optur/samplers/__init__.py
@@ -1,3 +1,4 @@
+from optur.samplers.builder import create_sampler
 from optur.samplers.sampler import Sampler
 
-__all__ = ["Sampler"]
+__all__ = ["Sampler", "create_sampler"]
---
+++ optur/samplers/builder.py
@@ -0,0 +1,6 @@
+from optur.proto.sampler_pb2 import SamplerConfig
+from optur.samplers import Sampler
+
+
+def create_sampler(sampler_config: SamplerConfig) -> Sampler:
+    pass
