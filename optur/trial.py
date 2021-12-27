a1aa6234a5f31e4142d4611c1c9fb8e0b876f903
Yusuke Tsuzuku <ytsmiling@gmail.com>
2021-12-28 08:32:28 +0900

    Define I/F of Trial.

--- optur/trial.py
+++ optur/trial.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Sequence, Union, overload
 
 from optur.proto.study_pb2 import ParameterValue
 from optur.proto.study_pb2 import Trial as TrialProto
@@ -9,12 +9,49 @@ class Trial:
     def __init__(self, trial_proto: TrialProto, storage: StorageClient) -> None:
         self._trial_proto = trial_proto
         self._storage = storage
+        self._fixed_parameters = {key: param for key, param in trial_proto.parameters.items()}
+        self._suggested_parameters: Dict[str, ParameterValue] = {}
+        self._used_parameters: Dict[str, ParameterValue] = {}
 
     @property
     def proto(self) -> TrialProto:
         return self._trial_proto
 
+    def suggest_int(self, name: str, low: int, high: int, *, log_scale: bool) -> int:
+        pass
+
+    def suggest_float(self, name: str, low: float, high: float, *, log_scale: bool) -> float:
+        pass
+
+    def suggest_categorical(
+        self, name: str, choices: Sequence[Union[int, float, str]]
+    ) -> Union[int, float, str]:
+        pass
+
+    @overload
+    def set_parameter(self, name: str, parameter: int, *, force: bool = False) -> int:
+        ...
+
+    @overload
+    def set_parameter(self, name: str, parameter: float, *, force: bool = False) -> float:
+        ...
+
+    @overload
+    def set_parameter(self, name: str, parameter: str, *, force: bool = False) -> str:
+        ...
+
+    def set_parameter(
+        self, name: str, parameter: Union[int, float, str], *, force: bool = False
+    ) -> Union[int, float, str]:
+        pass
+
+    def reset_parameter(self, name: str, *, force: bool) -> bool:
+        pass
+
     def update_parameters(self, parameters: Dict[str, ParameterValue]) -> None:
+        pass
+
+    def reset(self, *, hard: bool, reload: bool) -> None:
         pass
 
     def flush(self) -> None:
