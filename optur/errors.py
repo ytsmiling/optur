8026a2ad199e5e9aaac4e2da76b7bca5ac45a73a
Yusuke Tsuzuku <ytsmiling@gmail.com>
2021-12-27 12:11:37 +0900

    Add Exists/NotFound errors.

--- optur/errors.py
+++ optur/errors.py
@@ -1,2 +1,25 @@
+# The error codes.
+OK = 0
+ALREADY_EXISTS = 1
+NOT_FOUND = 2
+
+
 class PrunedException(Exception):
     pass
+
+
+class StatusError(Exception):
+    def __init__(self, message: str, error_code: int) -> None:
+        super().__init__(message)
+        self.message = message
+        self.error_code = error_code
+
+
+class AlreadyExistsError(StatusError):
+    def __init__(self, message: str) -> None:
+        super().__init__(message, ALREADY_EXISTS)
+
+
+class NotFoundError(StatusError):
+    def __init__(self, message: str) -> None:
+        super().__init__(message, NOT_FOUND)
