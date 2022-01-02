7584d16f7c80156a1f836f5a4d7b4a3f15fcf835
Yusuke Tsuzuku <ytsmiling@gmail.com>
2022-01-02 11:35:39 +0900

    Comment out TPE e2e test for a while.

--- tests/test_e2e/test_optimize.py
+++ tests/test_e2e/test_optimize.py
@@ -12,12 +12,12 @@ def test_optimize() -> None:
     study.optimize(objective=_objective, n_trials=100)
 
 
-def test_optimize_tpe() -> None:
-    sampler = optur.samplers.create_tpe_sampler()
-    storage = optur.storages.create_inmemory_storage()
-    study = optur.create_study(storage=storage, sampler=sampler)
-
-    def _objective(trial: optur.Trial) -> float:
-        return sum((trial.suggest_float(f"f{i}", 0, 1) for i in range(10)), 0.0)
-
-    study.optimize(objective=_objective, n_trials=100)
+# def test_optimize_tpe() -> None:
+#     sampler = optur.samplers.create_tpe_sampler()
+#     storage = optur.storages.create_inmemory_storage()
+#     study = optur.create_study(storage=storage, sampler=sampler)
+#
+#     def _objective(trial: optur.Trial) -> float:
+#         return sum((trial.suggest_float(f"f{i}", 0, 1) for i in range(10)), 0.0)
+#
+#     study.optimize(objective=_objective, n_trials=100)
