51496edbd483b1908782c64036b43ccf1f1712e8
Yusuke Tsuzuku <ytsmiling@gmail.com>
2022-01-03 18:11:09 +0900

    Add more mysql tests.

--- tests/test_storages/test_backends/test_mysql.py
+++ tests/test_storages/test_backends/test_mysql.py
@@ -4,7 +4,8 @@ import uuid
 
 import pytest
 
-from optur.proto.study_pb2 import StudyInfo, Target
+from optur.errors import NotFoundError
+from optur.proto.study_pb2 import AttributeValue, StudyInfo, Target, Trial
 from optur.storages.backends.mysql import MySQLBackend
 
 
@@ -70,3 +71,170 @@ def test_read_all_study() -> None:
     assert [(s.study_id, s.targets) for s in sorted_studies] == [
         (s.study_id, s.targets) for s in sorted_loaded_studies
     ]
+
+
+@pytest.mark.mysql
+def test_incremental_read_study() -> None:
+    backend = MySQLBackend(
+        user=os.environ["MYSQL_USER"],
+        host=os.environ["MYSQL_HOST"],
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        password=os.environ["MYSQL_PASSWORD"],
+        database=os.environ["MYSQL_DATABASE"],
+    )
+    backend.init()
+    backend.drop_all()
+    backend.init()
+    studies = [
+        StudyInfo(
+            study_id=uuid.uuid4().hex,
+            targets=[
+                Target(
+                    name=uuid.uuid4().urn,
+                    direction=random.choice(
+                        seq=[Target.Direction.MAXIMIZE, Target.Direction.MINIMIZE],
+                    ),
+                ),
+            ],
+        )
+        for _ in range(13)
+    ]
+    for study in studies[:6]:
+        backend.write_study(study=study)
+    timestamp = backend.get_current_timestamp()
+    for study in studies[6:]:
+        backend.write_study(study=study)
+    loaded_studies = [(s.study_id, s.targets) for s in backend.get_studies(timestamp=timestamp)]
+    for study in studies[6:]:
+        assert (study.study_id, study.targets) in loaded_studies
+
+
+@pytest.mark.mysql
+def test_read_write_trial() -> None:
+    backend = MySQLBackend(
+        user=os.environ["MYSQL_USER"],
+        host=os.environ["MYSQL_HOST"],
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        password=os.environ["MYSQL_PASSWORD"],
+        database=os.environ["MYSQL_DATABASE"],
+    )
+    backend.init()
+    backend.drop_all()
+    backend.init()
+    study = StudyInfo(study_id=uuid.uuid4().hex)
+    trial = Trial(
+        trial_id=uuid.uuid4().hex,
+        study_id=study.study_id,
+        system_attrs={"foo": AttributeValue(string_value="bar")},
+    )
+    backend.write_study(study=study)
+    backend.write_trial(trial=trial)
+    loaded_trial = backend.get_trial(trial_id=trial.trial_id)
+    assert dict(loaded_trial.system_attrs.items()) == {"foo": AttributeValue(string_value="bar")}
+
+
+@pytest.mark.mysql
+def test_write_trial_with_non_existent_study() -> None:
+    backend = MySQLBackend(
+        user=os.environ["MYSQL_USER"],
+        host=os.environ["MYSQL_HOST"],
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        password=os.environ["MYSQL_PASSWORD"],
+        database=os.environ["MYSQL_DATABASE"],
+    )
+    backend.init()
+    backend.drop_all()
+    backend.init()
+    study = StudyInfo(study_id=uuid.uuid4().hex)
+    trial = Trial(
+        trial_id=uuid.uuid4().hex,
+        study_id=uuid.uuid4().hex,
+        system_attrs={"foo": AttributeValue(string_value="bar")},
+    )
+    backend.write_study(study=study)
+    with pytest.raises(NotFoundError):
+        backend.write_trial(trial=trial)
+
+
+@pytest.mark.mysql
+def test_read_non_existent_trial() -> None:
+    backend = MySQLBackend(
+        user=os.environ["MYSQL_USER"],
+        host=os.environ["MYSQL_HOST"],
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        password=os.environ["MYSQL_PASSWORD"],
+        database=os.environ["MYSQL_DATABASE"],
+    )
+    backend.init()
+    backend.drop_all()
+    backend.init()
+    study = StudyInfo(study_id=uuid.uuid4().hex)
+    trial = Trial(
+        trial_id=uuid.uuid4().hex,
+        study_id=study.study_id,
+        system_attrs={"foo": AttributeValue(string_value="bar")},
+    )
+    backend.write_study(study=study)
+    backend.write_trial(trial=trial)
+    with pytest.raises(NotFoundError):
+        backend.get_trial(trial_id=uuid.uuid4().hex)
+
+
+@pytest.mark.mysql
+def test_read_trials_without_timestamp() -> None:
+    backend = MySQLBackend(
+        user=os.environ["MYSQL_USER"],
+        host=os.environ["MYSQL_HOST"],
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        password=os.environ["MYSQL_PASSWORD"],
+        database=os.environ["MYSQL_DATABASE"],
+    )
+    backend.init()
+    backend.drop_all()
+    backend.init()
+    study1 = StudyInfo(study_id=uuid.uuid4().hex)
+    study2 = StudyInfo(study_id=uuid.uuid4().hex)
+    trials1 = [Trial(trial_id=uuid.uuid4().hex, study_id=study1.study_id) for _ in range(6)]
+    trials2 = [Trial(trial_id=uuid.uuid4().hex, study_id=study2.study_id) for _ in range(7)]
+    backend.write_study(study=study1)
+    backend.write_study(study=study2)
+    for trial in trials1:
+        backend.write_trial(trial=trial)
+    for trial in trials2:
+        backend.write_trial(trial=trial)
+    loaded_trials = backend.get_trials(study_id=study1.study_id)
+    assert len(loaded_trials) == 6
+    assert set(t.trial_id for t in loaded_trials) == set(t.trial_id for t in trials1)
+    loaded_trials = backend.get_trials(study_id=study2.study_id)
+    assert len(loaded_trials) == 7
+    assert set(t.trial_id for t in loaded_trials) == set(t.trial_id for t in trials2)
+
+
+@pytest.mark.mysql
+def test_read_trial_with_timestamp() -> None:
+    backend = MySQLBackend(
+        user=os.environ["MYSQL_USER"],
+        host=os.environ["MYSQL_HOST"],
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        password=os.environ["MYSQL_PASSWORD"],
+        database=os.environ["MYSQL_DATABASE"],
+    )
+    backend.init()
+    backend.drop_all()
+    backend.init()
+    study1 = StudyInfo(study_id=uuid.uuid4().hex)
+    study2 = StudyInfo(study_id=uuid.uuid4().hex)
+    trials1 = [Trial(trial_id=uuid.uuid4().hex, study_id=study1.study_id) for _ in range(7)]
+    trials2 = [Trial(trial_id=uuid.uuid4().hex, study_id=study2.study_id) for _ in range(6)]
+    backend.write_study(study=study1)
+    backend.write_study(study=study2)
+    for trial in trials1[:2]:
+        backend.write_trial(trial=trial)
+    timestamp = backend.get_current_timestamp()
+    for trial in trials2:
+        backend.write_trial(trial=trial)
+    for trial in trials1[2:]:
+        backend.write_trial(trial=trial)
+    loaded_trials = backend.get_trials(study_id=study1.study_id, timestamp=timestamp)
+    assert len(loaded_trials) == 5
+    assert set(t.trial_id for t in loaded_trials) == set(t.trial_id for t in trials1[2:])
